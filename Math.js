ef043de030e31bd35aa1e67e8d2261082cc6b3e7
dhruvmayank <mayankdhruv9120@gmail.com>
2026-01-11 01:20:39 +0530

    learnt math functions

---
+++ Math.js
@@ -0,0 +1,44 @@
+//whenever new keyword is used then that means it will be created in heap memory
+
+//let num1 = 231;
+let num2 = new Number(231);
+let num3 = new Number(231);
+
+//console.log(num1 == num2); //true
+//console.log(num2 === num3); //false
+// console.log(num2);
+// console.log(typeof num2);
+
+let num = 231.68;
+// console.log(num.toFixed(1));
+// console.log(num.toPrecision(5));
+// console.log(num.toExponential(2));
+// console.log(num.toString());
+// console.log(num.valueOf());
+
+//Math Object
+
+// console.log(Math.E);
+// console.log(Math.LN10);
+// console.log(Math.PI);
+// console.log(Math.LOG10E);
+
+//floor and ceil
+let num1 = 23.9;
+// console.log(Math.floor(num1));
+// console.log(Math.ceil(num1));
+
+//round
+// console.log(Math.round(23.4));
+// console.log(Math.round(23.5));
+// console.log(Math.floor(Math.random() * 10));
+
+//generate 1-10
+
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+//11-20
+// console.log(Math.floor(Math.random() * 10) + 11);
+
+//min = 40 , max = 40
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
