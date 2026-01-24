e1928adce9cd08332ea9f87c3307a2eecc90a46e
dhruvmayank <mayankdhruv9120@gmail.com>
2026-01-24 23:31:39 +0530

    Intro to DOM

---
+++ DOM/lec1.js
@@ -0,0 +1,36 @@
+//DOM: document object model, means web page ka structure, manipulate karne ke liye use hota hai, coverts whole page to a js object
+
+//document.getElementById("first").innerHTML = "mahan aadmi";
+//document.getElementById("first").innerHTML = "mahan aadmi";=> this line means ki jisko bhi id first milega uska inner html mahan aadmi se replace kar dega
+//className: class ka name change karne ke liye use hota hai
+// const obj = document.getElementById("first");
+// obj.className = "Mohan";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Notes:
+//a. Accessing by ID: document.getElementById("id")
+//b. Accessing by Class Name: document.getElementsByClassName("className")
+//c.Accessing by css selector: document.querySelector("cssSelector")
+//d. Accessing by Tag Name: document.getElementsByTagName("tagName")
